65bd57a9726050420d556abe7a3fa864f562774e
Pudi-Sravan <sravanpudi8112005@gmail.com>
2024-06-21 03:43:18 +0530

    changed file structure all Front-end in a folder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "/src/main.jsx": "/src/Front_end/main.jsx",
+    },
+  },
 });
